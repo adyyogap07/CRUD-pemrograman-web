d70517e3c29b8e9f0505ccac8d1eb21c8e01d24e
AdyYoga <adyyoga07@gmail.com>
2024-05-29 21:27:01 +0700

    ternyata bener yang awal

--- koneksi.php
+++ koneksi.php
@@ -1,16 +1,48 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$db = "crud";
+class Database {
+    private $host = "localhost";
+    private $user = "root";
+    private $password = "";
+    private $db = "crud";
+    private $connection;
 
-// Establish connection
-$kon = mysqli_connect($host, $user, $password, $db);
 
-// Check connection
-if (!$kon) {
-    die("Koneksi gagal: " . mysqli_connect_error());
+    public function __construct() {
+        $this->connect();
+    }
+
+    private function connect() {
+        $this->connection = new mysqli($this->host, $this->user, $this->password, $this->db);
+
+        if ($this->connection->connect_error) {
+            die("Koneksi gagal: " . $this->connection->connect_error);
+        }
+    }
+
+    public function getConnection() {
+        return $this->connection;
+    }
+
+    public function query($sql) {
+        return $this->connection->query($sql);
+    }
+
+    public function fetchAssoc($result) {
+        return $result->fetch_assoc();
+    }
+
+    public function fetchAll($result) {
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function escapeString($string) {
+        return $this->connection->real_escape_string($string);
+    }
+
+    public function close() {
+        $this->connection->close();
+    }
 }
 
 ?>
